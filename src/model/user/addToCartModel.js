6200a1dd5f1c76bd1694c02150f6c2615bb18e50
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2024-09-18 14:43:08 +0530

    developmentChanges18/9/24

--- src/model/user/addToCartModel.js
+++ src/model/user/addToCartModel.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 const UserSchema = new mongoose.Schema({
   id: {
-    type: Number,
+    type: ObjectId,
     required: true,
   },
   amount: {
